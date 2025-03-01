cbe925348653de1335ee2867c2148bb79343a1dd
C. Mitch Carroll <c.mitch.carroll@gmail.com>
2025-03-01 17:17:00 -0600

    another try at documentation formatting

--- lib/roll.mli
+++ lib/roll.mli
@@ -1,7 +1,7 @@
 (** Fate Dice roller module *)
 (** A FATE die is a special 6-sided die (d6) that has 2 blank sides, 2 '+' sides, and 2 '-' sides.
- * The possible values for each FATE die are -1, 0, and +1. In effect, it is equivalent to 1d3-1.
- * The standard notation for a FATE die is 'dF'. Thus rolling 3 FATE dice is written as '3dF'. *)
+    The possible values for each FATE die are -1, 0, and +1. In effect, it is equivalent to 1d3-1.
+    The standard notation for a FATE die is 'dF'. Thus rolling 3 FATE dice is written as '3dF'. *)
 
 (** [df ()] Roll 1dF *)
 val df : unit -> int
@@ -10,6 +10,6 @@ val df : unit -> int
 val roll : int -> int
 
 (** [fate n] roll 4dF+n.
- * This is the standard FATE dice roll used in the FATE RPG rules. *)
+    This is the standard FATE dice roll used in the FATE RPG rules. *)
 val fate : int -> int
 
