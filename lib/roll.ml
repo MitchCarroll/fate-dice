e8e675d85fa481679cacbf7b53946aa20e475deb
C. Mitch Carroll <c.mitch.carroll@gmail.com>
2025-03-13 08:39:29 -0500

    added advantage/disadvantage rolls
    TODO: allow for variable levels of advantage
    FIXME: make the functions DRY

--- lib/roll.ml
+++ lib/roll.ml
@@ -1,6 +1,7 @@
 (* Fate Dice roller *)
 
 let () = Unix.time () |> Int.of_float |> Random.init
+
 let df () = Random.int 3 - 1
 
 let roll n =
@@ -11,3 +12,19 @@ let roll n =
   !count
 
 let fate n = n + roll 4
+
+let adv n = 
+    let bonus = match df () with
+    | (-1) -> 0
+    | 0
+    | 1    -> 1
+    | _ -> 0
+    in bonus + roll 3 + n
+
+let dis n =
+    let bonus = match df () with
+    | (-1)
+    | 0 -> (-1)
+    | 1 -> 0
+    | _ -> 0
+    in bonus + n + roll 3
--- lib/roll.mli
+++ lib/roll.mli
@@ -3,9 +3,6 @@
     The possible values for each FATE die are -1, 0, and +1. In effect, it is equivalent to 1d3-1.
     The standard notation for a FATE die is 'dF'. Thus rolling 3 FATE dice is written as '3dF'. *)
 
-(** [df ()] Roll 1dF *)
-val df : unit -> int
-
 (** [roll n] roll ndF *)
 val roll : int -> int
 
@@ -13,3 +10,13 @@ val roll : int -> int
     This is the standard FATE dice roll used in the FATE RPG rules. *)
 val fate : int -> int
 
+(** [adv n] roll 4dF+n with advantage.
+    Advantage means that one of the dice has its
+    value increased by 1 (but no more than the regular
+    max value of the die) *)
+val adv : int -> int
+
+(** [dis n] roll 4dF+n with disadvantage.
+    Disadvantage works like advantage, but
+    with the bonus die REDUCED by one value. *)
+val dis : int -> int
