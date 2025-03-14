db96e4d11d2c5f0ef4eaefa7122d8a5891c4c713
C. Mitch Carroll <c.mitch.carroll@gmail.com>
2025-03-13 23:12:27 -0500

    minor code cleanup

--- lib/roll.ml
+++ lib/roll.ml
@@ -4,19 +4,23 @@ let () = Unix.time () |> Int.of_float |> Random.init
 
 let df () = Random.int 3 - 1
 
-let da () = match df () with
-| (-1) -> 0
-| 0 
-| 1 -> 1
-| _ -> 0
+let da () = 
+    match df () with
+    | (-1) -> 0
+    | 0 
+    | 1 -> 1
+    | _ -> 0
 
-let dd () = match df () with
-| (-1) 
-| 0 -> (-1)
-| 1 -> 0
-| _ -> 0
+let dd () = 
+    match df () with
+    | (-1) 
+    | 0 -> (-1)
+    | 1 -> 0
+    | _ -> 0
 
-let rolls f n = List.init n (fun _ -> f ()) |> List.fold_left (+) 0
+let rolls f n = 
+    List.init n (fun _ -> f ()) 
+    |> List.fold_left (+) 0
 
 let roll n = rolls df n
 
